d07ad7466182d7845a499ee1077c4bcece70999b
vangjv <jonathan.vang@gmail.com>
2024-05-06 16:36:12 -0500

    modified endpoints to API management endpoints

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   region: 'eastus',
-  speechTokenEndpoint: "https://avafrontapi.azurewebsites.net/api/speech/token",
-  avaFrontApiEndpoint: "https://avafrontapi.azurewebsites.net/api",
+  speechTokenEndpoint: "https://avafront.azure-api.net/api/speech/token",
+  avaFrontApiEndpoint: "https://avafront.azure-api.net/api",
   // other environment-specific variables...
 };
